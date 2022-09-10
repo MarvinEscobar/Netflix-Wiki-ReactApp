49b004883422bfd20b3b0a9e1d186945d78ca734
Marvin Schoobaar <marvin.schoobaar@proact.nl>
2022-09-10 15:11:27 +0200

    Search bug fixed

--- src/Pages/Browse/Search.js
+++ src/Pages/Browse/Search.js
@@ -7,58 +7,66 @@ import SearchGallery from '../../Components/ProductComponents/SearchGallery';
 import { searchTitles } from '../../Data/RapidApi';
 
 function Search() {
-  const queryParams = new URLSearchParams(window.location.search);
-  const country_list = queryParams.get("country_list");
-  const limit = queryParams.get("limit");
-  const expiring = queryParams.get("expiring");
-  const type = queryParams.get("type");
-  const title = queryParams.get("title");
+  let queryParams = new URLSearchParams(window.location.search);
+  let country_list = queryParams.get("country_list");
+  let limit = queryParams.get("limit");
+  let expiring = queryParams.get("expiring");
+  let type = queryParams.get("type");
+  let title = queryParams.get("title");
 
   const history = useHistory();
   const { ...authState } = useContext(AuthenticationStateContext);
 
   let [data, setData] = useState(null);
+  console.log("Reloading");
 
   const handleNavigation = useCallback((path) => {
     history.push(path);
     history.goForward();
   }, [history]);
 
+  const fetchData = useCallback(async () => {
+
+    setData(null);
+
+    let params = {
+      order_by: 'date'
+    }
+
+    if (country_list)
+      params["country_list"] = [country_list];
+
+    if (limit)
+      params["limit"] = limit;
+
+    if (expiring)
+      params["expiring"] = expiring;
+
+    if (type)
+      params["type"] = type;
+
+    if (title)
+      params["title"] = title;
+
+    setData(await fetchDataAsync(params));
+  }, [country_list, expiring, limit, title, type]);
+
+  useEffect(() => {
+    return history.listen((location) => {
+      fetchData();
+    })
+  }, [history, fetchData])
+
   useEffect(() => {
     if (!authState.user) {
       handleNavigation(Routes.Login);
     }
     else {
-      const fetchData = async () => {
-        
-        setData(null);
-
-        let params = {
-          order_by: 'date'
-        }
-
-        if (country_list)
-          params["country_list"] = [country_list];
-
-        if (limit)
-          params["limit"] = limit;
-
-        if(expiring)
-          params["expiring"]=expiring;
-
-        if(type)
-          params["type"]=type;
-
-        if(title)
-          params["title"]=title;
-
-        setData(await fetchDataAsync(params));
-      };
 
       if (!data)
         fetchData();
     }
-  }, [setData, handleNavigation, data, authState, country_list, limit, expiring, type, title]);
+  }, [setData, handleNavigation, fetchData, data, authState]);
 
   async function fetchDataAsync(params) {
 
@@ -69,7 +77,7 @@ function Search() {
   return (
     <main className="browse">
       <section className="main-container">
-        <SearchGallery Id="search-all" Title={`Searchresults `} Count={data?.length??0} Data={data} />
+        <SearchGallery Id="search-all" Title={`Searchresults `} Count={data?.length ?? 0} Data={data} />
       </section>
     </main>
   );
